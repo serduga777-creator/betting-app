d34dbaae1e93feef0ba4900d931c0f7a87d650d0
serduga777-creator <serduga777@gmail.com>
2026-03-21 19:11:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// тестовый маршрут
 app.get("/", (req, res) => {
-  res.send("🚀 Betting app is running");
+  res.send("Betting app is running");
 });
 
-// порт (ВАЖНО для Railway)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server started on port " + port);
+  console.log(`Server started on port ${port}`);
 });
