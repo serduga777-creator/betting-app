6ea65fb252cdee5e27bcfbb48bdfc3dd436d54de
serduga777-creator <serduga777@gmail.com>
2026-03-23 16:18:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-require("dotenv").config();
-
-const express = require("express");
-const cors = require("cors");
-const pool = require("./db");
+nst pool = require("./db");
 
 const app = express();
 
