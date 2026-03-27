98646e4e714db2e7b5d28e11225f38ebc68214e7
serduga777-creator <serduga777@gmail.com>
2026-03-27 16:16:26 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = pool;
